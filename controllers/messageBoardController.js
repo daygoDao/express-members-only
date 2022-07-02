16f92bf11dc6b432779a49bb109e48d6a31406d3
dao vo <gary.vo32@gmail.com>
2022-07-01 21:14:29 -0700

    making index.ejs render base on if user is signed in or not

--- controllers/messageBoardController.js
+++ controllers/messageBoardController.js
@@ -1,5 +1,5 @@
 const async = require("async");
 
 exports.index = function (req, res) {
-  res.render("index", { title: "ayo from msgBoardController" });
+  res.render("index", { title: "Nerd Message Board", user: req.user });
 };
